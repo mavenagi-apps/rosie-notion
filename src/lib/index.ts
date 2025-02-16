ef1fae19a10db646908ed9352e2b09b34a03cd61
Sami Shalabi <sami@mavenagi.com>
2025-02-16 16:09:32 -0500

    clean up

--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { SAMPLE_ACTION_ID, installSampleAction, runSampleAction } from './actions';
-export { upsertSampleKnowledge, refreshSampleKnowledge } from './knowledge';
-export { SAMPLE_USERS } from './users';
-export { handleSampleTrigger } from './triggers';
